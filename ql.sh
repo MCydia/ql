30f407390cd3d3b746df3c7aa428ec127bd9dd7b
MCydia <61176750+MCydia@users.noreply.github.com>
2022-10-30 20:18:17 +0800

    Update ql.sh

--- ql.sh
+++ ql.sh
@@ -356,7 +356,7 @@ docker run -dit \
   --name qinglong \
   --hostname qinglong \
   --restart unless-stopped \
-  whyour/qinglong:latest
+  feverrun/qinglong:2.10.2
   
   docker restart qinglong > /dev/null 2>&1
   sleep 2
